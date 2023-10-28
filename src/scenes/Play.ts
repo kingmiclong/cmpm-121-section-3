5ec641d24101982a9de5d7809d24f8889f7b3f73
kingmiclong <b302057069@gmail.com>
2023-10-27 18:38:17 -0700

    Done with 2nd partner

--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -3,6 +3,35 @@ import starfieldUrl from '/assets/starfield.png';
 import shipUrl from '/assets/ship.png';
 import enemyShipUrl from '/assets/enemy.png';
 
+// Prototype for enemy types
+interface EnemyPrototype {
+  key: string;
+  speed: number;
+}
+
+const enemyTypes: Record<string, EnemyPrototype> = {
+  "fast": {
+    key: 'enemyShip',
+    speed: 5,
+  },
+  "slow": {
+    key: 'enemyShip',
+    speed: 2,
+  },
+  // ... add more enemy types
+};
+
+// Function to create enemy using prototype
+function createEnemy(scene: Phaser.Scene, x: number, y: number, type: string) {
+  const enemyPrototype = enemyTypes[type];
+  if (enemyPrototype) {
+    const enemy = scene.physics.add.sprite(x, y, enemyPrototype.key) as Phaser.Physics.Arcade.Sprite;
+    enemy.setData('speed', enemyPrototype.speed);
+    return enemy;
+  }
+  return null;
+}
+
 export default class Play extends Phaser.Scene {
   private fire?: Phaser.Input.Keyboard.Key;
   private left?: Phaser.Input.Keyboard.Key;
@@ -32,16 +61,29 @@ export default class Play extends Phaser.Scene {
     this.fire = this.addKey('F');
     this.left = this.addKey('LEFT');
     this.right = this.addKey('RIGHT');
-
+  
     this.starfield = this.add.tileSprite(0, 0, this.game.config.width as number, this.game.config.height as number, 'starfield').setOrigin(0, 0);
     this.ship = this.physics.add.sprite(320, 400, 'ship');  // Changed to physics.add.sprite
-
-    this.enemyShips = this.physics.add.group({
-      key: 'enemyShip',
-      repeat: 4,
-      setXY: { x: 100, y: 100, stepX: 100 }
-    });
-
+  
+    // Initialize enemy ships group
+    this.enemyShips = this.physics.add.group();
+    
+    // Create fast enemies
+    for (let i = 0; i < 5; i++) {
+      const enemy = createEnemy(this, 100 + i * 100, 100, "fast");
+      if (enemy) {
+        this.enemyShips.add(enemy);
+      }
+    }
+    
+    // Create slow enemies
+    for (let i = 0; i < 5; i++) {
+      const enemy = createEnemy(this, 200 + i * 100, 200, "slow");
+      if (enemy) {
+        this.enemyShips.add(enemy);
+      }
+    }
+  
     // Add collision detection
     this.physics.add.collider(
       this.ship!, 
@@ -52,7 +94,6 @@ export default class Play extends Phaser.Scene {
       undefined,  // Explicitly pass undefined
       this
     );
-    
   }
 
   update() {
@@ -73,14 +114,20 @@ export default class Play extends Phaser.Scene {
     // Move enemy ships
     this.enemyShips?.children.iterate((enemy: any) => {
       if (enemy) {
-        let typedEnemy = enemy as Phaser.GameObjects.Sprite;  // Type cast to Sprite
-        typedEnemy.x += 3;
+        let typedEnemy = enemy as Phaser.Physics.Arcade.Sprite;
+        const speed = typedEnemy.getData('speed');
+        typedEnemy.x += speed;
         if (typedEnemy.x > 800) {
           typedEnemy.x = -100;
         }
       }
-      return null;  // Return null to satisfy TypeScript
+      return null;
     });
+
+    // Reset the ship if it goes beyond the top edge of the screen
+    if (this.isShipLaunched && this.ship && this.ship.y < 0) {
+      this.resetShip();
+    }
 
     if (this.fire?.isDown && !this.isShipLaunched && this.ship) {
       this.isShipLaunched = true;
@@ -88,7 +135,15 @@ export default class Play extends Phaser.Scene {
     }
   }
 
+  private resetShip() {
+    this.ship?.setPosition(320, 400);
+    this.ship?.setVelocity(0, 0);
+    this.isShipLaunched = false;
+  }
+
   private handleCollision(ship: Phaser.Physics.Arcade.Sprite, enemy: Phaser.Physics.Arcade.Sprite) {
+    // Reset ship to original position
+    this.resetShip();
     // Reset ship to original position
     ship.setPosition(320, 400);
     ship.setVelocity(0, 0);
